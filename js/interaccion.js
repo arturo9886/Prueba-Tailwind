c83efff047133dd3547e9b282e86a7c37de7ece9
arturo9886 <45191921+arturo9886@users.noreply.github.com>
2025-06-12 02:37:44 -0400

    Add files via upload

---
+++ js/interaccion.js
@@ -0,0 +1,10 @@
+// empezaremos fuertes.
+// diosito cuidame pero E PA ARRIBA QUE VAMOS!
+
+const inputEmail = document.querySelector('#email');
+const inputAsunto = document.querySelector('#Asunto');
+const inputMensaje = document.querySelector('#Mensaje');
+
+console.log(inputEmail);
+console.log(inputAsunto);
+console.log(inputMensaje);
